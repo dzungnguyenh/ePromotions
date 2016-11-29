47c39560a84b5278d9f0b4f9cc6fd2a1c87dbb9a
ducxinh <xinhnd@asiantech.vn>
2016-11-29 13:57:58 +0700

    add migration products, categories, promotions

---
+++ database/migrations/2016_11_29_062506_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+             $table->increments('id');
+             $table->string('product_name',200);
+             $table->float('price');
+             $table->text('description');
+             $table->integer('quantity');
+             $table->integer('category_id')->unsigned();
+             $table->integer('user_id')->unsigned();
+             $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2016_11_29_063300_create_promotions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('percent');
+            $table->integer('quantity');
+            $table->datetime('date_start');
+            $table->datetime('date_end');
+            $table->integer('product_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
