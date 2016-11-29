86832bf0fa2f324b7565898ad5031ec851d7fd11
at-phuctv <phuctv@asiantech.com>
2016-11-29 21:54:39 +0700

    create database vouchers and vote_products

---
+++ database/migrations/2016_11_29_144128_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->integer('point');
+            $table->float('value');
+            $table->integer('user_id')->->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
---
+++ database/migrations/2016_11_29_144206_create_vote_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoteProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rank');
+            $table->integer('user_id')->unsigned();
+             $table->integer('product_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote_products');
+    }
+}
