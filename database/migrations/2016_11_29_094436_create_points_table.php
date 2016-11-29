c9c89d88d4e694874f136b55d86f2d0df32231bd
at-tungpt <tungpt@asiantech.vn>
2016-11-29 16:51:36 +0700

    add feature#38

---
+++ database/migrations/2016_11_29_094436_create_points_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->increments('id');
+            $tablr->integer('vote', 10);
+            $tablr->integer('share', 10);
+            $tablr->integer('book', 10);
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
