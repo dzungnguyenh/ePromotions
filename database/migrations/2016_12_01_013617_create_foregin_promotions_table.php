5f9aae4aa7b0a8a8f874fc34cba74becaacbf781
ducxinh <xinhnd@asiantech.vn>
2016-12-01 09:37:59 +0700

    add_foreignkey_promotion_vouchers_products

---
+++ database/migrations/2016_12_01_013617_create_foregin_promotions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForeginPromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('promotions', function (Blueprint $table) {
+            $table->foreign('product_id')
+            ->references('id')->on('products')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('promotions', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+        });
+    }
+}
