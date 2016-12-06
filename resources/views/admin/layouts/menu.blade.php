a4e791fa756a309cae8c950d7291fa1c549c6608
at-anhlq <anhlq@asiantech.vn>
2016-12-06 10:20:19 +0700

    Feature#29 add required and front end

---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,6 @@
+<li><a href="{!! url('point') !!}">Point</a>
+      <ul class="sub-menu">
+        <li><a href="{!! url('admin/point/create') !!}">Create</a></li>
+        <li><a href="{!! url('admin/point') !!}">List</a></li>
+      </ul>
+</li>
