4b1914b0f491f10c8d3601301cb31cdd0d69c9a3
ducxinh <xinhnd@asiantech.vn>
2016-12-07 17:30:35 +0700

    fix phpcs

--- app/Http/Middleware/ProductMiddleware.php
+++ app/Http/Middleware/ProductMiddleware.php
@@ -10,15 +10,16 @@ class ProductMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request Request
+     * @param \Closure                 $next    Next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if (auth()->check() && auth()->user()->role==2 {
+        if (auth()->check() && auth()->user()->role==2) {
             return $next($request);
         }
-        return redirect('business/login');
+            return redirect('business/login');
     }
 }
