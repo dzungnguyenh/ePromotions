47cb9f64bc9410692d29975350e033bae249401f
at-thanhtc <tranchithanhtkit@gmail.com>
2016-12-08 11:28:24 +0700

    feature#83_model controller

---
+++ config/constants.php
@@ -0,0 +1,5 @@
+<?php
+	return [
+    'ROLEADMIN' => '1',
+    'ROLEBUSSINESS' => '2',
+];
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $faker = Faker::create();
+      for ($i=1; $i < 4; $i++) {
+        Role::create([
+        	'role' => $faker->name,
+          'created_at' => $faker->dateTimeThisYear($max = 'now')
+        ]);
+      }
+    }
+}
