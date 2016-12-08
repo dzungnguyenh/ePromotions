26eacc06c14e343723a774d867e61985746115fe
at-phuctv <phuctv@asiantech.com>
2016-12-08 11:14:42 +0700

    Create Controller and Model feature#6

---
+++ app/Models/Event.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /**
+     * Define model.
+     *
+     * @var string
+     */
+    protected $table = 'events';
+     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id','title', 'description', 'start_time', 'end_time','place',
+    ];
+}
