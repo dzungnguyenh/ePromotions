e013b09b189a2b41db22e767354a1ae988c133d9
at-thanhtc <tranchithanhtkit@gmail.com>
2016-12-08 11:35:03 +0700

    feature#83_view resources

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+  <footer class="main-footer">
+    <!-- Default to the left -->
+    <strong>{!! trans('footer.copyRight') !!}<a href="#">{!! trans('header.ePromotions') !!}</a></strong>{!! trans('footer.allRight') !!}
+  </footer>
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 2.2.3 -->
+<script src="bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="bower_components/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="bower_components/AdminLTE/dist/js/app.min.js"></script>
+
+</body>
+</html>
