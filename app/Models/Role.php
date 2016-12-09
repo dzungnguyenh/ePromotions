74557800df595fd75041479a32a7309d4f825f26
at-thanhtc <tranchithanhtkit@gmail.com>
2016-12-09 09:44:05 +0700

    feature#83_fixed CI

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,23 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
 
-    protected $table = 'roles';
+  protected $table = 'roles';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['role'];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['role'];
 
-    /**
-     * Get the users for role
-     *
-     * @return User Information of users
-     */
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
+  /**
+   * Get the users for role
+   *
+   * @return User Information of users
+   */
+  public function users()
+  {
+      return $this->hasMany(User::class);
+  }
 
 }
