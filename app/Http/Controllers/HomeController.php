9032b02a7d63c795f957a62604fd1f6786fc1713
ducxinh <xinhnd@asiantech.vn>
2016-12-14 11:49:24 +0700

    fix phpcs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-        /**
-         * Create a new controller instance.
-         *
-         * @return void
-         */
-        public function __construct()
-        {
-            $this->middleware('auth');
-        }
+    /**
+    * Create a new controller instance.
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-        /**
-         * Show the application dashboard.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function index()
-        {
-            return view('admin.index');
-        }
+    /**
+    * Show the application dashboard.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        return view('admin.index');
+    }
 }
