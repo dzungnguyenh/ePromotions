7241a9ebb96974cdc37387aaa534b1546a107f06
at-anhlq <anhlq@asiantech.vn>
2016-12-16 12:45:53 +0700

    feature#4 forget password

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'it.leanh@gmail.com',
+        'name' => 'ePromotions reset password',
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -49,5 +49,5 @@ Route::get('/callback/{provider}', 'SocialAccountController@callback');
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
