2b91d07ffef61d63d6f03b6716eaefd83ecb530a
at-huentx <huentx@asiantech.com>
2016-12-21 09:45:30 +0700

    add index page

---
+++ resources/views/include/head.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>{{trans('header.title')}}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="{{asset('/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css')}}">
+<!-- font-awesome -->
+<link href="{{asset('/bower_components/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+<!-- Custom Theme files -->
+<link href="/css/style-home.css" rel='stylesheet' type='text/css' />
+
+</head>
+<body>
--- routes/web.php
+++ routes/web.php
@@ -50,12 +50,14 @@ Route::group(['middleware' => 'user', 'prefix' => 'user'],function()
     Route::resource('userorder','User\UserOrderController');
 });
 
-
 Route::get('/redirect/{provider}', 'SocialAccountController@redirect');
 Route::get('/callback/{provider}', 'SocialAccountController@callback');
 
 Auth::routes();
 
-
 Route::get('/', 'HomeController@index');
 Route::get('/product', 'HomeController@product');
+
+Route::get('/category/{id}', 'Product\ProductController@showByIdCategory');
+
+Route::get('/logout', 'UserController@logout');
